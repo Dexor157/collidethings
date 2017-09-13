fbe32c654e165e2940126eeedfc25102d6ebc69d
Sean <Sean@LAPTOP-315CAAIO.lakeheadu.ca>
2017-09-13 14:36:37 -0400

    commit the cube again

--- drawcube/src/drawcube/Drawcube.java
+++ drawcube/src/drawcube/Drawcube.java
@@ -21,7 +21,12 @@ public class Drawcube extends JFrame {
     length = Math.sqrt(Math.abs(((x2 - x1)^2) + ((y2 - y1)^2)));
     return length;
     }
+    public double howlong(int width, int height){
+    double length;
+    length = Math.sqrt(Math.pow(width,2) + (Math.pow(height,2)));
     
+    return length;
+    }
     
     
     public void paint(Graphics g) {
@@ -30,11 +35,13 @@ public class Drawcube extends JFrame {
         int y = 400;
         int width = 150;
         int height = 150;
-        double angle = 45;
-        double slantlength = howlong(x, x+width, y, y-height);
+        double angle = Math.PI/4;
+        //double slantlength = howlong(width, height);
+        double slantlength = width;
         System.out.println(slantlength);
         int slantwidth = (int)(slantlength * Math.cos(angle));
         int slantheight = (int)(slantlength * Math.sin(angle));
+        
         System.out.println(slantwidth);
         System.out.println(slantheight);
         Graphics2D g2 = (Graphics2D) g;
@@ -43,9 +50,11 @@ public class Drawcube extends JFrame {
         Line2D lin0 = new Line2D.Float(x, y, x-width,y);
         Line2D lin1 = new Line2D.Float(x, y, x, y-height);
         Line2D lin2 = new Line2D.Float(x, y, x+slantwidth, y-slantheight);
+        Line2D lin3 = new Line2D.Float(x, y-height, x-width, y-height);
         g2.draw(lin0);
         g2.draw(lin1);
         g2.draw(lin2);
+        g2.draw(lin3);
     }
     
     public static void main(String[] args) {
